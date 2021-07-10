3fc2f81807da86fb9b72218b0f3d537feb0493e7
mognedy <mognedy@gmail.com>
2021-07-10 15:51:08 +0200

    Validate , Create user and fireUnverifiedUserCreatedEvent.

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const redisClient = require("./redis-client");
+const userSchema = require("./user-validation-schema");
 
 const CREATE_UNVERIFIED_USER_STREAMS_KEY =
   "registration:create_unverified_user";
+
+const UNVERIFIED_USER_CREATED_STREAMS_KEY =
+  "registration:unverified_user_created";
 
 const processMessage = (results) => {
   const [, messages] = results[0]; // `key` equals to registration:create_user
@@ -20,6 +24,31 @@ const processMessage = (results) => {
   };
 };
 
+const prepairUserDataForInserting = (user) => {
+  const data = [];
+  for (const property in user) {
+    data.push(property, user[property]);
+  }
+  return data;
+};
+
+const fireUnverifiedUserCreatedEvent = async (id) => {
+  redisClient.rawCallAsync([
+    "xadd",
+    UNVERIFIED_USER_CREATED_STREAMS_KEY,
+    "*",
+    "id",
+    id,
+  ]);
+};
+
+const addUser = async (user) => {
+  const data = prepairUserDataForInserting(user);
+  const id = await redisClient.rawCallAsync(["incr", "users"]);
+  await redisClient.rawCallAsync(["hmset", `user:${id}`, "id", id, ...data]);
+  fireUnverifiedUserCreatedEvent(id);
+};
+
 const listenForMessage = async (key, lastId = "$") => {
   // `results` is an array, each element of which corresponds to a key.
   const results = await redisClient.rawCallAsync([
@@ -33,6 +62,11 @@ const listenForMessage = async (key, lastId = "$") => {
 
   const { id, user } = processMessage(results);
 
+  const { error, value } = userSchema.validate(user);
+
+  if (!error) {
+    addUser(value);
+  }
   // Pass the last id of the results to the next round.
   await listenForMessage(key, id);
 };
